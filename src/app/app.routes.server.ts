1f6f49ecfe5586c7863bcfebf9724973911a78e8
unknown <atamossat@gmail.com>
2026-01-17 22:29:42 +0100

    Added 404 error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'about',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Server
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
